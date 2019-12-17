a060b55f652968d667c803cad9cb0de8e166321b
Nethelbert <gilblasse@gmail.com>
2019-12-17 18:08:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  
+  get '/' do 
+    erb:index
+  end
+  
+  post '/greet' do 
+    redirect '/greet'
+  end
+  
+  get '/greet' do
+    binding.pry
+  end
 
 end
